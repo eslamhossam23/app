c64749397ad5dc7e9c6da161f46e5022a2fe44a2
eslamhossam23 <eslamhossam23@gmail.com>
2016-11-09 21:51:53 +0100

    Added popup menu resource file, added method for showing the popup in main activity.

--- src/main/java/com/timprojet/affichagedinformationssuruneimage/MainActivity.java
+++ src/main/java/com/timprojet/affichagedinformationssuruneimage/MainActivity.java
@@ -5,6 +5,11 @@ import android.graphics.BitmapFactory;
 import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.PopupMenu;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +17,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void showPopup(View v) {
+        PopupMenu popup = new PopupMenu(this, v);
+        MenuInflater inflater = popup.getMenuInflater();
+        inflater.inflate(R.menu.menu, popup.getMenu());
+        popup.show();
     }
 }
